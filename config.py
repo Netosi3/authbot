28fc8cc8e9a8478902bbe3791fdb92317e3d2447
Netosi3 <166701050+Netosi3@users.noreply.github.com>
2024-06-29 09:37:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-DISCORD_TOKEN         = 'your-token'
+DISCORD_TOKEN         = 'MTI1NjQ5MTkzMjY2Njk1Nzk0Nw.GPDdrT.NpMR5F_Oo1QWNVrAkZlAfnmr1qjIt6WsaGC-Bw'
 cPREFIX               = '!'                                   # Префикс команды бота
 embedColor            = 0x8c4bc9                              # Цвет плашки в дискорде
 
 launcher = dict()
-launcher['MUSTDIE']   = 'http://your.link/Launcher.jar'       # Ваша ссылка на windows версию лаунчера
-launcher['LINUX/MAC'] = 'http://your.link/Launcher.exe'       # А здесь на linux/macos версию
+launcher['MUSTDIE']   = 'http://34.34.26.22:9274/Prestarter.jar'       # Ваша ссылка на windows версию лаунчера
+launcher['LINUX/MAC'] = 'http://34.34.26.22:9274/Prestarter.exe'       # А здесь на linux/macos версию
 
 db = dict()
 db['host']            = 'localhost'                           # Хост mysql/mariadb
@@ -15,7 +15,7 @@ db['db_name']         = 'db'                                  # База
 scm = dict()
 scm['skindir']        = './skins'                             # Путь к папке со скинами
 scm['capedir']        = './capes'                             # Путь к папке с плащами
-scm['url']            = 'http://your.link/skinapi'            # Url из конфика nginx
+scm['url']            = 'http://34.34.26.22:9274/skinapi'            # Url из конфика nginx
 scm['host']           = 'localhost'                           # Хост
 scm['port']           = 8123                                  # Порт
 scm['defaultSkin']    = './defaults/skin.png'                 # Путь к default скину
