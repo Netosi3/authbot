515a21fd5f4c0bd02a215945e2756a482eaca4e1
Netosi3 <166701050+Netosi3@users.noreply.github.com>
2024-06-29 09:29:15 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/timoxa0/SDLB-Gravit
-cd SDLB-Gravit
+git clone https://github.com/Netosi3/authbot.git
+cd authbot
 pip install -r requirements.txt
 chmod +x main.py *.sh
