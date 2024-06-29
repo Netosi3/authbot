d821e107f861fd7f0c40cf0547ce1c1aab925036
Netosi3 <166701050+Netosi3@users.noreply.github.com>
2024-06-29 09:28:00 +0300

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='authbot',
+    version='2.0',
+    packages=[''],
+    url='https://github.com/Netosi3/authbot.git',
+    license='GNU General Public License v3.0',
+    author='Netosi3',
+    author_email='',
+    description='Authentication bot for WarmixMC'
+)
